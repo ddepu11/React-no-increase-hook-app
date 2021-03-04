19b92ffe11d8ca6532ae54f32d7a6d684c8ac61c
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-04 17:07:01 +0530

    Some files deleted and Style added and no increase functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  let [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>{count}</h2>
+      <button onClick={increaseCount}>Increase the Number</button>
     </div>
   );
-}
+};
 
 export default App;
